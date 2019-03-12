f73979150eefc5f6ac1744df3b268c4525998f49
EfrenSango <efren.sanchezgon@gmail.com>
2019-03-12 16:14:28 +0100

    Updates

---
+++ precache-manifest.60301b65621fee4bd0cfb9960cb30016.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6bdb1b4e2f0fa9654301",
+    "url": "/react-todolist/static/css/main.f5904fe6.chunk.css"
+  },
+  {
+    "revision": "6bdb1b4e2f0fa9654301",
+    "url": "/react-todolist/static/js/main.6bdb1b4e.chunk.js"
+  },
+  {
+    "revision": "a6dd1dd57ec7ad0dd537",
+    "url": "/react-todolist/static/js/1.a6dd1dd5.chunk.js"
+  },
+  {
+    "revision": "15e18c78eb78a1dce817",
+    "url": "/react-todolist/static/js/runtime~main.15e18c78.js"
+  },
+  {
+    "revision": "f93b86660fe2de549d5e877a860b2dc5",
+    "url": "/react-todolist/static/media/gato.f93b8666.svg"
+  },
+  {
+    "revision": "665f529992e7816ab674909f6c767fc8",
+    "url": "/react-todolist/index.html"
+  }
+];
